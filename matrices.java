ca722d9e7749e887cb354bb31eaccfa47f3717a1
Jabbar <saj8180025653@gmail.com>
2023-09-19 12:48:57 +0530

    First commit from VS Code

--- matrices.java
+++ matrices.java
@@ -23,7 +23,7 @@ public class matrices{
             }
         }
 
-        //output        
+        //output of matrix 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 System.out.print(matrix[i][j]+ " ");
