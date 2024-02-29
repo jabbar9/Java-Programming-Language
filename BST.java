74df4569faf5e058334c803c51b25d38996af677
Jabbar <saj8180025653@gmail.com>
2024-02-29 18:30:20 +0530

    Delete Program Added

--- BST.java
+++ BST.java
@@ -43,18 +43,55 @@ public class BST {
             return search(root.right, key);
         }
     }
+    public static Node delete(Node root, int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        } else if(root.data > val){
+            root.left = delete(root.left, val);
+        } else {
+            //Case 1:- Leaf Node
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            //Case 2:- Single Child
+            if(root.left == null){
+                return root.right;
+            } else if(root.right == null){
+                return root.left;
+            }
+
+            //Case 3:- Both Children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
     public static void main(String[] args){
-        int values[] = {5,1,3,4,2,7};
+        //int values[] = {5,1,3,4,2,7};
+        int values[] = {8, 5,3,1,4,6,10,11,14};
         Node root = null;
         for(int i=0; i<values.length; i++){
             root = insert(root, values[i]);
         }
         inorder(root);
         System.out.println();
-        if(search(root,1)){
-            System.out.print("Found");
-        } else {
-            System.out.print("Not Found");
-        }
+        // if(search(root,1)){
+        //     System.out.print("Found");
+        // } else {
+        //     System.out.print("Not Found");
+        // }
+
+        root = delete(root, 10);
+        System.out.println();
+
+        inorder(root);
     }
 }
